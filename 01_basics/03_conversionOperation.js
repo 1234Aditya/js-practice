a8dcd37c096fde1152c096e7c012a3b9fa742c15
Aditya Sethi <99201246+1234Aditya@users.noreply.github.com>
2023-10-25 17:54:01 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,7 +7,7 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = 0;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true , 2 => false
 //"" => false
@@ -15,5 +15,29 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************Operations******************
+let value = 3;
+let negVal = -value;
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Aditya Sethi"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+ 2);    //  12
+// console.log(1+ "2");    //  12
+// console.log("1"+2+2);   //  122
+// console.log(1+2+"2");   //  32
+
+console.log(+true);
+console.log(+"");
