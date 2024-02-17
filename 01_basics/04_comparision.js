d8e7e534dc2c3bca3adce4fede5dd78e7b65200a
Aditya Sethi <sethiaditya152@gmail.com>
2024-02-17 12:18:36 +0530

    datatypes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -15,3 +15,22 @@
 // ===
 
 console.log("2" === 2);
+
+//   Primitive datatypes
+/*
+    7 types:
+        String
+        Number
+        Boolean
+        null
+        undefined
+        Symbol
+        BigInt
+*/
+
+//Reference(Non-Primitive)
+/*
+    Array
+    Objects
+    Functions
+*/ 
